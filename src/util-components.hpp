d0eb15a63f1f4066a44ee86c8162b43f55c290c6
forenoonwatch <91013521+forenoonwatch@users.noreply.github.com>
2019-10-05 16:31:43 -0400

    Added motion component to util components

--- src/util-components.hpp
+++ src/util-components.hpp
@@ -1,5 +1,13 @@
 #pragma once
 
+#include <engine/math/matrix.hpp>
+
 struct TransformComponent {
 	Matrix4f transform;
 };
+
+struct MotionComponent {
+	Vector3f velocity;
+	Vector3f acceleration;
+};
+
