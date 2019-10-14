589cd04c1fede60e56e84d4ad660ec2f8f7ab7a0
forenoonwatch <91013521+forenoonwatch@users.noreply.github.com>
2019-10-14 17:42:47 -0400

    Removed block.cpp

--- src/block.cpp
+++
@@ -1,9 +0,0 @@
-#include "block.hpp"
-
-ArrayList<BlockInfo> BlockInfo::blockInfo;
-
-void BlockInfo::registerType(enum BlockType type, uint32 flags, IndexedModel& model,
-		VertexArray& vertexArray, Material& material) {
-	blockInfo.push_back(BlockInfo(type, flags, &model, &vertexArray, &material));
-}
-
