4a17e9d6eb766dfd0dbfe99f0313cac2982bee7c
forenoonwatch <91013521+forenoonwatch@users.noreply.github.com>
2019-10-17 15:18:02 -0400

    Changed block info back to having a static list of data (minus VAO)

---
+++ src/block.cpp
@@ -0,0 +1,23 @@
+#include "block.hpp"
+
+#include <engine/rendering/indexed-model.hpp>
+#include <engine/rendering/vertex-array.hpp>
+#include <engine/rendering/material.hpp>
+
+#include <engine/rendering/render-context.hpp>
+
+ArrayList<BlockInfo> BlockInfo::blockInfo;
+
+void BlockInfo::registerType(enum BlockInfo::BlockType type, uint32 flags,
+		IndexedModel* model, Material* material, float mass, float volume) {
+	blockInfo.push_back(BlockInfo(type, flags, model, material, mass, volume));
+}
+
+void BlockInfo::initVertexArrays(RenderContext& renderContext,
+		ArrayList<Memory::SharedPointer<VertexArray>>& vaos) {
+	for (auto& info : blockInfo) {
+		vaos.emplace_back(new VertexArray(renderContext, *info.model,
+				GL_STATIC_DRAW));
+	}
+}
+
--- src/block.hpp
+++ src/block.hpp
@@ -2,6 +2,7 @@
 
 #include <engine/core/common.hpp>
 #include <engine/core/memory.hpp>
+#include <engine/core/array-list.hpp>
 
 #include <engine/math/quaternion.hpp>
 
@@ -9,53 +10,65 @@ class IndexedModel;
 class VertexArray;
 class Material;
 
-struct BlockInfo {
-	enum BlockType {
-		TYPE_BASIC_CUBE,
-		TYPE_BASIC_TETRA,
-		TYPE_BASIC_PYRAMID,
-		TYPE_BASIC_WEDGE,
-		TYPE_BASIC_FIVE_SIXTH,
-		TYPE_BASIC_WEDGE_2X1,
-		TYPE_BASIC_WEDGE_2X2,
+class RenderContext;
 
-		NUM_TYPES
-	};
+class BlockInfo {
+	public:
+		enum BlockType {
+			TYPE_BASIC_CUBE,
+			TYPE_BASIC_TETRA,
+			TYPE_BASIC_PYRAMID,
+			TYPE_BASIC_WEDGE,
+			TYPE_BASIC_FIVE_SIXTH,
+			TYPE_BASIC_WEDGE_2X1,
+			TYPE_BASIC_WEDGE_2X2,
 
-	enum Flags {
-		FLAG_OCCLUDES	= 0b1
-	};
+			NUM_TYPES
+		};
 
-	inline BlockInfo()
-			: type((enum BlockType)0)
-			, flags(0)
-			, model(nullptr)
-			, vertexArray(nullptr)
-			, material(nullptr)
-			, mass(0.f)
-			, volume(0.f) {}
+		enum Flags {
+			FLAG_OCCLUDES	= 0b1
+		};
 
-	inline BlockInfo(enum BlockType type, uint32 flags, IndexedModel* model,
-				Material* material,
-				Memory::SharedPointer<VertexArray> vertexArray, float mass,
-				float volume)
-			: type(type)
-			, flags(flags)
-			, model(model)
-			, vertexArray(vertexArray)
-			, material(material)
-			, mass(mass)
-			, volume(volume) {}
+		static void registerType(enum BlockInfo::BlockType type, uint32 flags,
+				IndexedModel* model, Material* material, float mass,
+				float volume);
 
-	enum BlockType type;
-	uint32 flags;
+		static void initVertexArrays(RenderContext& renderContext,
+				ArrayList<Memory::SharedPointer<VertexArray>>& vaos);
 
-	IndexedModel* model;
-	Material* material;
-	Memory::SharedPointer<VertexArray> vertexArray;
+		static inline BlockInfo& getInfo(enum BlockInfo::BlockType type) {
+			return blockInfo[(uintptr)type];
+		}
 
-	float mass;
-	float volume;
+		enum BlockType type;
+		uint32 flags;
+
+		IndexedModel* model;
+		Material* material;
+
+		float mass;
+		float volume;
+	private:
+		inline BlockInfo()
+				: type((enum BlockType)0)
+				, flags(0)
+				, model(nullptr)
+				, material(nullptr)
+				, mass(0.f)
+				, volume(0.f) {}
+
+		inline BlockInfo(enum BlockType type, uint32 flags,
+					IndexedModel* model, Material* material, float mass,
+					float volume)
+				: type(type)
+				, flags(flags)
+				, model(model)
+				, material(material)
+				, mass(mass)
+				, volume(volume) {}
+
+		static ArrayList<BlockInfo> blockInfo;
 };
 
 struct Block {
