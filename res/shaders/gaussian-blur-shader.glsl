1865d284ab4b6188b28b8248c20749f5268f46ad
forenoonwatch <91013521+forenoonwatch@users.noreply.github.com>
2019-12-30 12:07:35 -0500

    Reverted gaussian blur shader

--- res/shaders/gaussian-blur-shader.glsl
+++ res/shaders/gaussian-blur-shader.glsl
@@ -14,31 +14,21 @@ void main() {
 	const ivec2 x = ivec2(gl_GlobalInvocationID.xy);
 
 	if (horizontal) {
-		const ivec2 size = imageSize(buffer0);
-
 		vec3 result = imageLoad(buffer0, x).rgb * weight[0];
 
-		for (int i = x.x + 1; i < size.x && i < x.x + 5; ++i) {
-			result += imageLoad(buffer0, ivec2(i, x.y)).rgb * weight[i];
-		}
-
-		for (int i = x.x - 1; i > 0 && i > x.x - 5; --i) {
-			result += imageLoad(buffer0, ivec2(i, x.y)).rgb * weight[i];
+		for (int i = 1; i < 5; ++i) {
+			result += imageLoad(buffer0, x + ivec2(i, 0)).rgb * weight[i];
+			result += imageLoad(buffer0, x - ivec2(i, 0)).rgb * weight[i];
 		}
 
 		imageStore(buffer1, x, vec4(0.5 * result, 1.0));
 	}
 	else {
-		const ivec2 size = imageSize(buffer1);
-
 		vec3 result = imageLoad(buffer1, x).rgb * weight[0];
 
-		for (int i = x.y + 1; i < size.y && i < x.y + 5; ++i) {
-			result += imageLoad(buffer1, ivec2(x.x, i)).rgb * weight[i];
-		}
-
-		for (int i = x.y - 1; i >= 0 && i > x.y - 5; --i) {
-			result += imageLoad(buffer1, ivec2(x.x, i)).rgb * weight[i];
+		for (int i = 1; i < 5; ++i) {
+			result += imageLoad(buffer1, x + ivec2(0, i)).rgb * weight[i];
+			result += imageLoad(buffer1, x - ivec2(0, i)).rgb * weight[i];
 		}
 
 		imageStore(buffer0, x, vec4(result, 1.0));
