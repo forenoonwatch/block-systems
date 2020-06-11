d6e9aa96a367a54cb75536e9a9599d824691e3b8
forenoonwatch <91013521+forenoonwatch@users.noreply.github.com>
2020-06-11 00:21:39 -0400

    Fixed gaussian blur shader

--- res/shaders/gaussian-blur-shader.glsl
+++ res/shaders/gaussian-blur-shader.glsl
@@ -1,39 +1,41 @@
+#include "common.glh"
 
-#if defined(CS_BUILD)
+#if defined(VS_BUILD)
 
-layout (local_size_x = 16, local_size_y = 16) in;
+layout (location = 0) in vec2 position;
 
-layout (binding = 0, rgba32f) uniform image2D buffer0;
-layout (binding = 1, rgba32f) uniform image2D buffer1;
+void main() {
+	gl_Position = vec4(position, 0.0, 1.0);
+}
+
+#elif defined(FS_BUILD)
+
+uniform sampler2D image;
 
 uniform bool horizontal;
 
 const float weight[] = {0.227027, 0.1945946, 0.1216216, 0.054054, 0.016216};
 
+layout (location = 0) out vec4 outColor;
+
 void main() {
-	const ivec2 x = ivec2(gl_GlobalInvocationID.xy);
+	const ivec2 x = ivec2(gl_FragCoord.xy);
+	vec3 result = texelFetch(image, x, 0).rgb * weight[0];
 
 	if (horizontal) {
-		vec3 result = imageLoad(buffer0, x).rgb * weight[0];
-
 		for (int i = 1; i < 5; ++i) {
-			result += imageLoad(buffer0, x + ivec2(i, 0)).rgb * weight[i];
-			result += imageLoad(buffer0, x - ivec2(i, 0)).rgb * weight[i];
+			result += texelFetch(image, x + ivec2(i, 0), 0).rgb * weight[i];
+			result += texelFetch(image, x - ivec2(i, 0), 0).rgb * weight[i];
 		}
-
-		imageStore(buffer1, x, vec4(0.5 * result, 1.0));
 	}
 	else {
-		vec3 result = imageLoad(buffer1, x).rgb * weight[0];
-
 		for (int i = 1; i < 5; ++i) {
-			result += imageLoad(buffer1, x + ivec2(0, i)).rgb * weight[i];
-			result += imageLoad(buffer1, x - ivec2(0, i)).rgb * weight[i];
+			result += texelFetch(image, x + ivec2(0, i), 0).rgb * weight[i];
+			result += texelFetch(image, x - ivec2(0, i), 0).rgb * weight[i];
 		}
-
-		imageStore(buffer0, x, vec4(result, 1.0));
 	}
+	
+	outColor = vec4(result, 1.0);
 }
 
 #endif
-
